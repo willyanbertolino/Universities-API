7b6c603b06df30462cb921f1deef17a084f46762
Willyan Bertolino <willyanhpbertolino@gmail.com>
2022-05-07 14:29:32 -0300

    university schema and populate script

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## MongoDB connection
 
-create a environment variable with name: MONGO_URI and insert the secret connection key.
+create an environment variable with name MONGO_URI and insert the secret connection key of the database.
+
+## Insert/Reset database
+
+To populate the database or reset to initial populated state run the script: node populate_db.js
---
+++ models/universities.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const universitySchema = new mongoose.Schema({
+  alpha_two_code: {
+    type: String,
+    required: [true, 'University code (two letter code) must be provide'],
+  },
+  web_pages: [],
+  name: {
+    type: String,
+    required: [true, 'University name must be provide'],
+  },
+  country: {
+    type: String,
+    required: [true, 'The country name must be provide'],
+  },
+  domains: [],
+  state_province: {
+    type: String,
+    default: null,
+  },
+});
+
+module.exports = mongoose.model('Universities', universitySchema);
